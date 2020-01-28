654d1824182b5d517c65e3bdd7040b9706360ebb
Jack Gerrits <jackgerrits95@gmail.com>
2020-01-27 21:12:08 -0500

    Remove usage of imported

--- libs/header.cmake
+++ libs/header.cmake
@@ -1,16 +1,16 @@
 # Define the header-only Boost target
-add_library(Boost::boost INTERFACE IMPORTED GLOBAL)
+add_library(boost_headers INTERFACE)
 
 # Disable autolink
-target_compile_definitions(Boost::boost INTERFACE BOOST_ALL_NO_LIB=1)
+target_compile_definitions(boost_headers INTERFACE BOOST_ALL_NO_LIB=1)
 
 if(BOOST_MODULARIZED)
   file(GLOB alllibs "${BOOST_SOURCE}/libs/*" "${BOOST_SOURCE}/libs/numeric/*")
   foreach(lib IN LISTS alllibs)
     if(IS_DIRECTORY "${lib}/include")
-      target_include_directories(Boost::boost SYSTEM INTERFACE "${lib}/include")
+      target_include_directories(boost_headers SYSTEM INTERFACE "${lib}/include")
     endif()
   endforeach()
 else()
-  target_include_directories(Boost::boost SYSTEM INTERFACE ${BOOST_SOURCE})
+  target_include_directories(boost_headers SYSTEM INTERFACE ${BOOST_SOURCE})
 endif()
